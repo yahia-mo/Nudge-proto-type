7fadfb6d8100adef16cb2011a87c46e81eec3e1d
Shahd Alhindawy <shahdazb98@gmail.com>
2026-09-19 15:58:37 +0300

    Enable CORS for frontend integration

--- main.py
+++ main.py
@@ -1,9 +1,18 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from src.api.routes.sessions import router as sessions_router
-
 from src.core.database import init_db
 
 app = FastAPI(title="NUDGE API")
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://127.0.0.1:5500"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 app.include_router(sessions_router)
 
 init_db()
